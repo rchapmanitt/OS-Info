fe5d2c255091947a0237a10ebb958d101690571d
Robert Chapman <68128014+rchapmanitt@users.noreply.github.com>
2024-10-26 23:56:56 -0500

    Add files via upload

---
+++ os.js
@@ -0,0 +1,10 @@
+const os = require( 'os' );
+console.log("\n");
+console.log("-------------------------------------------------");
+console.log("         Operating System Information            ");
+console.log("-------------------------------------------------");
+console.log(os.version(), "|", os.release(), "|", os.type(), "|", os.machine());
+console.log("-------------------------------------------------\n");
+
+
+
